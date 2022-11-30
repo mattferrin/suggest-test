752a227a879a0159782d48f9f0e840e81389f154
Matt Ferrin <mattferrin@gmail.com>
2022-11-30 01:30:14 -0800

    tested inclusion

--- src/buildReadCloverXml/ts/buildIncludeKind.ts
+++ src/buildReadCloverXml/ts/buildIncludeKind.ts
@@ -3,17 +3,21 @@ import { SummaryItem } from "./undefinedOrSummary";
 
 export function buildIncludeKind(input: MeowOutput) {
   return function includeKind(file: SummaryItem | undefined) {
-    const isStatementKeeper =
-      (file?.statements ?? 0) > 1 && (file?.uncoveredStatements ?? 0) > 0;
-    const isConditionalKeeper =
-      (file?.conditionals ?? 0) > 1 && (file?.uncoveredConditionals ?? 0) > 0;
-
-    if (!input.flags.includeConditionals) {
-      return isStatementKeeper;
-    } else if (!input.flags.includeStatements) {
-      return isConditionalKeeper;
+    if (file === undefined) {
+      return false;
     } else {
-      return isStatementKeeper || isConditionalKeeper;
+      const isStatementKeeper =
+        (file.statements ?? 0) > 0 && file.uncoveredStatements > 0;
+      const isConditionalKeeper =
+        (file.conditionals ?? 0) > 0 && file.uncoveredConditionals > 0;
+
+      if (!input.flags.includeConditionals) {
+        return isStatementKeeper;
+      } else if (!input.flags.includeStatements) {
+        return isConditionalKeeper;
+      } else {
+        return isStatementKeeper || isConditionalKeeper;
+      }
     }
   };
 }
