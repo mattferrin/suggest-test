9754a9c1cb309392389690905c6eda7d16960112
Matt Ferrin <mattferrin@gmail.com>
2022-11-29 20:53:48 -0800

    added first unit test example and husky runs tests

---
+++ babel.config.js
@@ -0,0 +1,9 @@
+/* eslint-disable functional/no-expression-statement */
+/* eslint-disable functional/immutable-data */
+
+module.exports = {
+  presets: [
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    "@babel/preset-typescript",
+  ],
+};
